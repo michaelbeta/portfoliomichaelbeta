0f8ceebb3b3571a671ae0d3c265dc575ec68143d
michaelbeta <betancourtmoran@gmail.com>
2023-02-21 23:01:01 -0600

    Se agregan proyectos

--- components/proyecto/galeriaDeProyecto.js
+++ components/proyecto/galeriaDeProyecto.js
@@ -2,16 +2,16 @@ import React from 'react'
 import Proyecto from "./tarjetaDeProyecto"
 import Styled from "../../styles/proyecto/GaleriaDeProyectos.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub,faPython } from "@fortawesome/free-brands-svg-icons";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import Tecnolgias from "../../models/proyecto/tecnologiaUsadas";
 import { Element } from 'react-scroll'
 import ImagenProyecto1 from "../../images/proyecto/proyecto 1/Carrusel rectangular.png"
+import ImagenProyecto2 from "../../images/proyecto/proyecto 2/Cartelera_peliculas.png"
+import ImagenProyecto3 from "../../images/proyecto/proyecto 3/conversor.png"
 
 const galeriaDeProyecto = () => {
 
-    const nombresDeTecnologias = ["Nextjs", "css", "javascript"];
-    const tecnologiasImplementadas = Tecnolgias(nombresDeTecnologias);
     return (
         <Element id='Projectos' name='Projectos'>
             <section id={Styled.galeriaDeProyectos}>
@@ -31,25 +31,41 @@ const galeriaDeProyecto = () => {
                         icono2={<FontAwesomeIcon icon={faArrowUpRightFromSquare} />}
                         url2={"https://carrusel-en-next-js.vercel.app/"}
                         //Tecnologia iplementada
-                        tecnologiasImplementadas={tecnologiasImplementadas}
+                        tecnologiasImplementadas={Tecnolgias(["Nextjs", "css", "javascript"])}
 
                     />
                     <Proyecto
                         //Imagen
-                        src={ImagenProyecto1}
-                        alt="Carruseles"
+                        src={ImagenProyecto2}
+                        alt="Cartelera"
                         width={550}
                         height={350}
                         //Info del proyecto
-                        TituloProyecto={"Diseño de carruseles"}
-                        descripcion={"Diseños de carruseles en 3D"}
+                        TituloProyecto={"Cartelera de películas"}
+                        descripcion={"Cartelera de películas-Proyecto Final"}
                         icono1={<FontAwesomeIcon icon={faGithub} />}
-                        url1={"https://github.com/michaelbeta/carrusel-en-next-js"}
+                        url1={"https://github.com/michaelbeta/ProyectoFinal"}
                         icono2={<FontAwesomeIcon icon={faArrowUpRightFromSquare} />}
-                        url2={"https://carrusel-en-next-jjmq15noj-michaelbeta.vercel.app/"}
+                        url2={"https://michaelbeta.github.io/ProyectoFinal/index.html"}
                         //Tecnologia iplementada
-                        tecnologiasImplementadas={tecnologiasImplementadas}
+                        tecnologiasImplementadas={Tecnolgias(["css", "javascript"])}
 
+                    />
+                    <Proyecto
+                        //Imagen
+                        src={ImagenProyecto3}
+                        alt="Conversor de bases"
+                        width={550}
+                        height={350}
+                        //Info del proyecto
+                        TituloProyecto={"Conversor de bases"}
+                        descripcion={"Conversor de bases en python"}
+                        icono1={<FontAwesomeIcon icon={faGithub} />}
+                        url1={"https://github.com/michaelbeta/Conversor-de-bases-en-Python"}
+                        icono2={<FontAwesomeIcon icon={faArrowUpRightFromSquare} />}
+                        url2={""}
+                        //Tecnologia iplementada
+                        tecnologiasImplementadas={Tecnolgias(["python"])}
                     />
                 </div>
             </section>
--- models/sobreMi/skills.js
+++ models/sobreMi/skills.js
@@ -1,13 +1,15 @@
 //Creamos la conexion a la carpeta
 const reqSvgs = require.context ( '../../images/skills', true, /.svg$/ );
-
+//
+const noIncluir=["./python","./postgresql"];
+//
 //funcion que regresa los path y images
 const svgMap =()=>{
 
   let pathSVG=[];
   const imagenesSVg=reqSvgs.keys().reduce((images, path) => {
     const key = path.substring(path.lastIndexOf("http://localhost:3000/home/") + 1, path.lastIndexOf('.'))//path
-    pathSVG.push(key);//guardamos el path
+    if(noIncluir.indexOf(key)==-1) pathSVG.push(key);//guardamos el path
     images[key] = reqSvgs(path).default//creamos la conexion al svg
     return images
   }, {})
